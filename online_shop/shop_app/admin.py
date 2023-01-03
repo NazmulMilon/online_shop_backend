3e0318a5857bf6f4b2272c674b3b35183512c318
nazmulmilon <milonsarker73@gmail.com>
2023-01-04 00:26:41 +0600

    shop_app models added

--- online_shop/shop_app/admin.py
+++ online_shop/shop_app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Category, Book, Product
+
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['category_title']
+    # fields = '__all__'
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class BookAdmin(admin.ModelAdmin):
+    exclude = ['updated_at']
+    # fields = '__all__'
+
+
+admin.site.register(Book, BookAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    exclude = ['updated_at']
+    # fields = '__all__'
+
+
+admin.site.register(Product, ProductAdmin)
--- online_shop/shop_app/models.py
+++ online_shop/shop_app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    category_title = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'categories'
+
+
+class Book(models.Model):
+    book_title = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=15)
+    pages = models.IntegerField()
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    description = models.TextField()
+    image_url = models.URLField()
+    status = models.BooleanField(default=True)
+    created_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+
+    class Meta:
+        # ordering = '-created_at'
+        db_table = 'books'
+
+
+class Product(models.Model):
+    product_tag = models.CharField(max_length=20)
+    product_name = models.CharField(max_length=200)
+    category =  models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_price = models.IntegerField()
+    stock = models.IntegerField()
+    image_url = models.URLField()
+    quantity = models.IntegerField()
+    description = models.TextField()
+    status = models.BooleanField(default=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        # ordering = '-created_at'
+        db_table = 'products'
