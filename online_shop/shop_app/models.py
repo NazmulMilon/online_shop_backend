bfd646656a342fb6331367b87d973279f0b5428f
nazmulmilon <milonsarker73@gmail.com>
2023-01-04 09:56:52 +0600

    shop_app models changed

--- online_shop/shop_app/models.py
+++ online_shop/shop_app/models.py
@@ -9,11 +9,15 @@ class Category(models.Model):
     class Meta:
         db_table = 'categories'
 
+    def __str__(self):
+        return self.category_title
+
 
 class Book(models.Model):
     book_title = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     isbn = models.CharField(max_length=15)
+    author_name = models.CharField(max_length=150, blank=True)
     pages = models.IntegerField()
     book_price = models.IntegerField()
     stock = models.IntegerField()
@@ -24,8 +28,11 @@ class Book(models.Model):
     update_at = models.DateField(auto_now=True)
 
     class Meta:
-        # ordering = '-created_at'
+        ordering = ['-created_at']
         db_table = 'books'
+
+    def __str__(self):
+        return '{0} {1}'.format(self.book_title, self.author_name)
 
 
 class Product(models.Model):
@@ -35,12 +42,14 @@ class Product(models.Model):
     product_price = models.IntegerField()
     stock = models.IntegerField()
     image_url = models.URLField()
-    quantity = models.IntegerField()
     description = models.TextField()
     status = models.BooleanField(default=True)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
     class Meta:
-        # ordering = '-created_at'
+        ordering = ['-created_at']
         db_table = 'products'
+
+    def __str__(self):
+        return '{} {}'.format(self.product_tag, self.product_name)
